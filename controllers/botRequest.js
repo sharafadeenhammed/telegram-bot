7b87ffe1332a2255fa6e16888ccb7de5f1e27882
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-09-14 23:42:45 +0100

    structured folder structure

---
+++ controllers/botRequest.js
@@ -0,0 +1,6 @@
+import asyncHAndeler from "express-async-handler";
+const readBotRequest = asyncHAndeler(async (req, res, next) => {});
+
+export default {
+  readBotRequest,
+};
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
 
 const server = express();
+dotenv.config();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 server.use(morgan("dev"));
 
 const PORT = process.env.PORT || 5000;
-server.listen();
+server.listen(PORT, () => {
+  console.log("server running on port ", PORT);
+});
